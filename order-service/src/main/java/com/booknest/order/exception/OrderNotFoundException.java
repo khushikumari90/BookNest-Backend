96a3f0fac722a1f0a7380a0de80836616788bf19
khushikumari90 <khushirawat2525@gmail.com>
2026-05-17 09:47:47 +0530

    make some changes

--- order-service/src/main/java/com/booknest/order/exception/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.booknest.order.exception;
-
-public class OrderNotFoundException extends RuntimeException {
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
