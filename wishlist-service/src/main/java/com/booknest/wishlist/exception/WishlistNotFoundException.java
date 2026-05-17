a5250e5c523186d65457cfc078f005d84b6bc3bb
khushikumari90 <khushirawat2525@gmail.com>
2026-05-17 09:49:38 +0530

    make some changes

--- wishlist-service/src/main/java/com/booknest/wishlist/exception/WishlistNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.booknest.wishlist.exception;
-
-public class WishlistNotFoundException extends RuntimeException {
-    public WishlistNotFoundException(String message) {
-        super(message);
-    }
-}
