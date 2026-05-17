e8cae5af8f27cdb3c5948019fbf9c479a6a207ad
khushikumari90 <khushirawat2525@gmail.com>
2026-05-17 09:48:43 +0530

    make some changes

--- notification-service/src/main/java/com/booknest/notification/exception/NotificationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.booknest.notification.exception;
-
-public class NotificationNotFoundException extends RuntimeException {
-    public NotificationNotFoundException(String message) {
-        super(message);
-    }
-}
